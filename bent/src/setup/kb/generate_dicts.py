95ed7277fefcd387937dadf6aec420357e49ae36
Pedro Ruas <32177425+pedroruas18@users.noreply.github.com>
2023-02-03 15:44:51 +0000

    Update generate_dicts.py

--- bent/src/setup/kb/generate_dicts.py
+++ bent/src/setup/kb/generate_dicts.py
@@ -5,7 +5,7 @@ import orjson as json
 import os
 from bent.src.setup.kb.kb import KnowledgeBase
 
-dict_dir = cfg.root_path + 'data/kbs/dicts/'
+dict_dir = cfg.root_path + 'data/dicts/'
 
 
 def generate_dicts_4_kb(kb=None, mode='reel', terms_filename=None, 
