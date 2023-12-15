e3388e7a3d501215f557bbe2dbb9abb8607ee2cb
Pedro Ruas <32177425+pedroruas18@users.noreply.github.com>
2023-12-15 17:37:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bent",
-    version="0.0.25",
+    version="0.0.27",
     description = "BENT: Biomedical Entity Annotator",
     long_description=open("README.rst").read(),
     author = "Pedro Ruas",
